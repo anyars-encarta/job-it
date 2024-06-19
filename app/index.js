60301f571654eab785d23d85a3f4587bc2dff560
anyars-encarta <anyarsencarta@gmail.com>
2024-06-19 10:27:39 +0000

    Implement top section of navbar

--- app/index.js
+++ app/index.js
@@ -18,11 +18,20 @@ const Home = () => {
                     <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%'/>
                 ),
                 headerRight: () => (
-                    <ScreenHeaderBtn iconUrl={icons.profile} dimension='100%'/>
+                    <ScreenHeaderBtn iconUrl={images.profile} dimension='100%'/>
                 ),
                 headerTitle: ''
             }}
             />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{ flex: 1, padding: SIZES.medium}}>
+                    <Welcome />
+
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
