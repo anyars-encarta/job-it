3c7cc59abcbad6fefa5c11fe5584f83e93b10f52
anyars-encarta <anyarsencarta@gmail.com>
2024-06-20 10:00:50 +0000

    Create a fetch hook

---
+++ hook/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { REACT_NATIVE_XRAPID_API_KEY } from '@env';
+
+const rapidApiKey = REACT_NATIVE_XRAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState('false');
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            'x-rapidapi-key': rapidApiKey,
+            'x-rapidapi-host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query },
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true)
+
+        try {
+            const response = await axios.request(options)
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (e) {
+            setError(e);
+            alert('There is an error, please try again later.')
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const refetch = () => {
+        setIsLoading(true)
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch };
+}
