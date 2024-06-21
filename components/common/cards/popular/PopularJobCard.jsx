c2e2c1da903785b4f7007ff626ed327a5e59ced5
anyars-encarta <anyarsencarta@gmail.com>
2024-06-21 17:31:28 +0000

    Add checkImageURL util

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,6 +1,8 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 
-import styles from './popularjobcard.style'
+import styles from './popularjobcard.style';
+
+import { checkImageURL } from '../../../../utils';
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
 
@@ -10,7 +12,11 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
     >
 
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
-        <Image source={{ uri: item.employer_logo }}
+        <Image source={{
+          uri: checkImageURL(item.employer_logo)
+            ? item.employer_logo
+            : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg'
+        }}
           resizeMode='contain'
           style={styles.logoImage}
         />
@@ -22,7 +28,7 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
         <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
           {item.job_title}
         </Text>
-        
+
         <Text style={styles.location}>{item.job_country}</Text>
       </View>
     </TouchableOpacity>
