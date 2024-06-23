dc323a79b4e96b88f3d6288ec20d5dfc107826d3
anyars-encarta <anyarsencarta@gmail.com>
2024-06-23 12:08:46 +0000

    Implementing job details page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,8 +1,41 @@
-import { Text } from 'react-native';
+import { 
+    Text, 
+    View, 
+    SafeAreaView, 
+    ScrollView, 
+    ActivityIndicator, 
+    RefreshControl
+} from 'react-native';
+import { Stack, useRouter, useSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
+
+import { 
+    Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics 
+} from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import { useFetch } from '../../hook/useFetch';
 
 const JobDetails = () => {
+    const params  = useSearchParams();
+    const router = useRouter();
+
+    const {data, isLoading, error, refetch} = useFetch('job-details', {job_id: params.id});
+
   return (
-    <Text>Job Details</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
+        <Stack.Screen
+        options={{
+            headerStyle: { backgroundColor: COLORS.lightWhite },
+            headerShadowVisible: false,
+            headerBackVisible: false,
+            headerLeft: () => (
+                <ScreenHeaderBtn />
+            )
+        }}
+        >
+
+        </Stack.Screen>
+    </SafeAreaView>
   )
 }
 
--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -5,7 +5,6 @@ import styles from './nearbyjobcard.style';
 import { checkImageURL } from '../../../../utils';
 
 const NearbyJobCard = ({ job, handleNavigate }) => {
-
   return (
     <TouchableOpacity style={styles.container} onPress={handleNavigate}>
       <TouchableOpacity style={styles.logoContainer}>
